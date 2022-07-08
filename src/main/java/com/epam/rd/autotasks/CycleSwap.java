da96c7c56414889983fee47eefffc6dd27a954b9
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-08 16:01:55 +0300

    CycleSwap

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,17 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        if (array!=null && array.length!=0) {
+            int rightMostValue = array[array.length - 1];
+            System.arraycopy(array, 0, array, 1, array.length - 1);
+            array[0] = rightMostValue;
+        }
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        if (array!=null && array.length!=0 && shift>0)
+            for (int i = 0; i < shift; i++) {
+                cycleSwap(array);
+            }
     }
 }
